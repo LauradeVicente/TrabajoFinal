a4fb180b21c537bb463ad60cbcf56703978dc635
LauradeVicente <lauradevicente99@gmail.com>
2021-10-30 17:30:41 +0200

    adding products data

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,15 +1,55 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
+	"sap/ui/model/FilterType"
 ],
-	/**
-	 * @param {typeof sap.ui.core.mvc.Controller} Controller
-	 */
-	function (Controller) {
+	
+
+	function (Controller, JSONModel, Filter, FilterOperator, FilterType) {
 		"use strict";
 
 		return Controller.extend("restaurant.finalproject.controller.App", {
-			onInit: function () {
 
+			onInit: async function () {
+				await this.initModels();
+				await this.loadModelsData();
+			},
+			
+			initModels: function () {
+				this.getView().setModel(new JSONModel(), "coldStorages");
+				this.getView().setModel(new JSONModel(), "incidents");
+				this.getView().setModel(new JSONModel(), "products");
+				this.getView().setModel(new JSONModel(), "vendors");
+			},
+
+			loadModelsData: async function () {
+				await this.loadStoragesModel();
+				await this.loadIncidentsModel();
+				await this.loadProductsModel();
+				await this.loadVendorsModel();
+			},
+
+			loadStoragesModel: async function () {
+				const oStoragesModel = this.getView().getModel("coldStorages");
+				await oStoragesModel.loadData("json/ColdStorages.json");
+			},
+
+			loadIncidentsModel: async function () {
+				const oIncidentsModel = this.getView().getModel("incidents");
+				await oIncidentsModel.loadData("json/Incidents.json");
+			},
+
+			loadProductsModel: async function () {
+				const oStoragesModel = this.getView().getModel("products");
+				await oStoragesModel.loadData("json/Products.json");
+			},
+
+			loadVendorsModel: async function () {
+				const oStoragesModel = this.getView().getModel("vendors");
+				await oStoragesModel.loadData("json/Vendors.json");
 			}
+
 		});
 	});
