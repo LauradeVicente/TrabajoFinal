7ab7efcae6bbf4fe5a362a611db2da67eeba8f70
Laura de Vicente <l.de.vicente.yanez@accenture.com>
2021-11-19 08:15:45 +0100

    Adding generic tag to Detail view

--- webapp/controller/ProductDetail.controller.js
+++ webapp/controller/ProductDetail.controller.js
@@ -1,17 +1,38 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"restaurant/finalproject/util/Formatter"
-	
+	"restaurant/finalproject/util/Formatter",
+	"sap/ui/core/Fragment"
 ],
 	
 
-	function (Controller, Formatter) {
+	function (Controller, Formatter,  Fragment) {
 		"use strict";
 
 		return Controller.extend("restaurant.finalproject.controller.ProductDetail", {
 
 			onInit: function () {
+				this.getOwnerComponent().get
+			},
+
+			onGenericTagPress: function (oEvent) {
+				let oView = this.getView();
+				let oSourceControl = oEvent.getSource();
+
+				if (!this._pPopover) {
+					Fragment.load({
+						id: oView.getId(),
+						name: "restaurant.finalproject.fragment.products.SalesDetail",
+						controller: this
+					}).then(function (oPopover) {
+						this._pPopover = oPopover;
+						oView.addDependent(this._pPopover);
+						this._pPopover.openBy(oSourceControl);
+					}.bind(this));
+				} else {
+					this._pPopover.openBy(oSourceControl);
+				}
 			}
+	
 
         });
     }
